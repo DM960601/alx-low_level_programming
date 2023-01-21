8b894d27db2d88ea63c3a298098ea8f2a4d54211
Duncan Momberume <deetuso@gmail.com>
2023-01-21 03:01:14 -0800

    Argc-argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	total = strtol(argv[1], &p, 10);
 	count = 0;
 
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 				}
 			}
 		}
-		if (total ==1)
+		if (total == 1)
 			count++;
 	}
 	else
