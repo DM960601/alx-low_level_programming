99324c4c31315c916848d9e7bc17ea82c948ed08
Duncan Momberume <deetuso@gmail.com>
2023-01-24 03:08:22 -0800

    Argc Argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,13 +8,12 @@
  * @argv: arguments
  * Return: 0
  */
-
 int main(int argc, char **argv)
 {
 	int total, count;
 	unsigned int i;
 	char *p;
-	int cents[] = {25, 10, 5, 2}
+	int cents[] = {25, 10, 5, 2};
 
 	if (argc != 2)
 	{
